ad9044f8c0b2e3002d47968631f3fba0b4a00865
intrusions <xeloronkali@gmail.com>
2023-12-22 17:56:39 +0100

    not compile

--- srcs/flag/manage_flag.c
+++ srcs/flag/manage_flag.c
@@ -6,7 +6,7 @@
 /*   By: xel <xel@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 23:32:41 by xel               #+#    #+#             */
-/*   Updated: 2023/12/22 01:43:31 by xel              ###   ########.fr       */
+/*   Updated: 2023/12/22 17:56:11 by xel              ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -29,7 +29,27 @@ static void print_man() {
     );
 }
 
-static bool is_valid_option(const char *option) {
+static u64 what_option_bit(const char *options) {
+    
+    if (!strcmp(options, "--debug-syms"))       { return FLAG_A; }
+    if (!strcmp(options, "--extern-only"))      { return FLAG_G; }
+    if (!strcmp(options, "--no-sort"))          { return FLAG_P; }
+    if (!strcmp(options, "--undefined-only"))   { return FLAG_U; }
+    if (!strcmp(options, "--reverse-sort"))     { return FLAG_R; }
+    return 0;
+}
+
+static u64 what_flag_bit(const char flag) {
+    
+    if (flag == 'a')     { return FLAG_A; }
+    if (flag == 'g')     { return FLAG_G; }
+    if (flag == 'p')     { return FLAG_P; }
+    if (flag == 'u')     { return FLAG_U; }
+    if (flag == 'r')     { return FLAG_R; }
+    return 0;
+}
+
+static bool is_valid_option(const char *option, u64 *flags) {
     
     const char *valid_options[N_FLAGS] = {
         "--debug-syms",
@@ -41,13 +61,14 @@ static bool is_valid_option(const char *option) {
 
     for (u8 i = 0; i < N_FLAGS; i++) {
         if (!strcmp(option, valid_options[i])) {
+            *flags |= what_option_bit(valid_options[i]);
             return true;
         }
     }
     return false;
 }
 
-static bool is_valid_option_flag(const char *option, char *invalid_flag) {
+static bool is_valid_option_flag(const char *option, char *invalid_flag, u64 *flags) {
     
     const char *valid_flags = "agpur";
 
@@ -57,6 +78,7 @@ static bool is_valid_option_flag(const char *option, char *invalid_flag) {
         for (u8 j = 0; valid_flags[j]; j++) {
             if (valid_flags[j] == option[i]) {
                 valid = true;
+                *flags |= what_flag_bit(option[i]);
                 break;
             }
         }
@@ -69,8 +91,7 @@ static bool is_valid_option_flag(const char *option, char *invalid_flag) {
     return true; 
 }
 
-
-bool manage_flag(int argc, char **argv) {
+bool manage_flag(int argc, char **argv, u64 *flags) {
 
     int     n_param = argc - 1;
     char    **options = argv + 1;
@@ -82,13 +103,13 @@ bool manage_flag(int argc, char **argv) {
 
     for (u16 i = 0; i < n_param; i++) {
         if (options[i][0] == '-' && options[i][1] == '-') {
-            if (!is_valid_option(options[i])) {
+            if (!is_valid_option(options[i], flags)) {
                 printf("nm: unrecognized option '%s'\n", options[i]);
                 print_man();
                 return false;
             }
         } else if (options[i][0] == '-') {
-            if (!is_valid_option_flag(options[i] + 1, &invalid_flag)) {
+            if (!is_valid_option_flag(options[i] + 1, &invalid_flag, flags)) {
                 printf("nm: unrecognized option '%c'\n", invalid_flag);
                 print_man();
                 return false;
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: xel <xel@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 23:22:32 by xel               #+#    #+#             */
-/*   Updated: 2023/12/22 01:58:52 by xel              ###   ########.fr       */
+/*   Updated: 2023/12/22 17:43:26 by xel              ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,16 +14,18 @@
 
 int main(int argc, char **argv) {
 
+    u64 flags;
+
     if (argc == 1) {                                        // Search a.out by default without flag
         nm("a.out");
     } else if (argv[argc - 1][0] == '-') {                  // Search a.out by default with flag
-        if (manage_flag(argc, argv)) {
+        if (manage_flag(argc, argv, &flags)) {
             nm("a.out");
         }
     } else if (argv[argc - 1][0] != '-' && argc == 2) {     // Search personal binary without flag
         nm(argv[argc - 1]);
     } else if (argv[argc - 1][0] != '-' && argc > 2) {      // Search personal binary with flag
-        if (manage_flag(argc, argv)) {
+        if (manage_flag(argc, argv, &flags)) {
             nm(argv[argc - 1]);
         }
     }
